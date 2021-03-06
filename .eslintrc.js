b05ebfd478708b3169ac0a379496f315fdf5faae
Devesh Mhatre <deveshmhtr.mhatre92@gmail.com>
2021-03-06 21:38:45 +0530

    Set up Prettier and ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+}
