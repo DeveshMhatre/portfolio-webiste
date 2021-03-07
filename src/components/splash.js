f02715046d90529527ea048e9232d299d307ef4b
Devesh Mhatre <deveshmhtr.mhatre92@gmail.com>
2021-03-07 11:03:24 +0530

    Add GSAP to the workflow
    - Animate splash to disappear using GSAP

--- src/components/splash.js
+++ src/components/splash.js
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { gsap } from 'gsap'
 
 import SiteLogo from './site-logo'
 
 const Splash = () => {
-  gsap.to('.progress', { width: '100%', duration: 1 })
+  const splash = useRef(null)
+  const progressBar = useRef(null)
+  const progress = useRef(null)
+
+  useEffect(() => {
+    gsap.to(progress.current, {
+      width: '100%',
+      duration: 0.5,
+      ease: 'power3.out',
+    })
+    gsap.to(progressBar.current, {
+      opacity: 0,
+      y: -5,
+      duration: 0.5,
+      delay: 0.5,
+    })
+    gsap.to(splash.current, {
+      display: 'none',
+      opacity: 0,
+      duration: 0.5,
+      delay: 1,
+    })
+  }, [])
 
   return (
-    <section className="splash">
+    <section ref={splash} className="splash">
       <div className="splash__content">
         <SiteLogo />
-        <div className="splash__content--loader">
-          <div className="progress"></div>
+        <div ref={progressBar} className="splash__content--loader">
+          <div ref={progress} className="progress"></div>
         </div>
       </div>
     </section>
