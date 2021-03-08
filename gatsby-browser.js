6ed6abcecceb4da230bc470c33dfae9d68391573
Devesh Mhatre <deveshmhtr.mhatre92@gmail.com>
2021-03-08 23:14:56 +0530

    Make Thank You and Not Found pages redirect only

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,14 @@
+import { navigate } from 'gatsby'
+
 import './src/styles/index.scss'
+
+// Prevent navigating to these pages unless redirected
+export const onInitialClientRender = () => {
+  if (
+    window.location.pathname.startsWith('/thankyou') ||
+    window.location.pathname.startsWith('/404') ||
+    window.location.pathname.startsWith('/seo')
+  ) {
+    navigate('/')
+  }
+}
